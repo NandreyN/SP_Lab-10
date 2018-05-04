4b758fb70a00b4a985ff5b49c6cee42699c89961
Андрей <andrey.naumovich@mail.ru>
2018-05-04 21:47:41 +0300

    Fixed issue with color after moving to another page

--- js/menu.js
+++ js/menu.js
@@ -6,4 +6,10 @@ $(document).ready(function () {
         var id = $(e.target).attr('id');
         $("#" + id).css("background", "red");
     });
+    $('a').click(function () {
+        var names = ["a-task", "b-task", "c-task"];
+        for (i = 0; i < names.length; i++) {
+            $("#" + names[i]).css("background", "red");
+        }
+    });
 });
