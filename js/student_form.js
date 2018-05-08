c8236cbdd3e6cbb368a245d5b4c6144b8b5bfbb4
Андрей <andrey.naumovich@mail.ru>
2018-05-08 08:19:26 +0300

    image upload handling/bug fixes

--- js/student_form.js
+++ js/student_form.js
@@ -2,24 +2,50 @@ $(document).ready(function () {
     $("#student_input").on("submit", function (event) {
         var surname = $("#surname").val();
         var email = $("#email").val();
-        var news = $("#news").val();
+        var news = $("#agree").attr("checked") == 'checked';
         var age = $("#ageCategory").val();
+
         if (surname === "" || email === "" || news === "") {
             event.preventDefault();
+            alert("Fill in the fields");
             return;
+        }
+
+        var genderRadio = $("[name='gender']");
+        var selected;
+        for (var i = 0; i < genderRadio.length; i++) {
+            if (genderRadio[i].checked)
+                selected = genderRadio[i].value;
         }
 
         var record = {
             'surname': surname,
             'email': email,
             'news': news,
-            'age': age
+            'age': age,
+            'gender': selected
         };
         sessionStorage.setItem('student', record);
-        alert("Record added");
+        alert("Record added : " + JSON.stringify(record));
     });
 
     $("#student_input").on("reset", function () {
+        $("#list").html("");
         alert("Cleared");
     });
+
+    function handleFileSelect(evt) {
+        var files = evt.target.files; 
+        
+        var output = [];
+        for (var i = 0, f; f = files[i]; i++) {
+            output.push('<li><strong>', escape(f.name), '</strong> (', f.type || 'n/a', ') - ',
+                f.size, ' bytes, last modified: ',
+                f.lastModifiedDate ? f.lastModifiedDate.toLocaleDateString() : 'n/a',
+                '</li>');
+        }
+        document.getElementById('list').innerHTML = '<ul>' + output.join('') + '</ul>';
+    }
+
+    document.getElementById('files').addEventListener('change', handleFileSelect, false);
 });
